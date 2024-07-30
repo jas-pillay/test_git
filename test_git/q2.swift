72b90359fafa4b972ed5d1eb24d02ad9d0776b2b
jasmyn <jasmynpillay22@gmail.com>
2024-07-30 10:01:04 -0400

    First commit

---
+++ test_git/q2.swift
@@ -0,0 +1,18 @@
+//
+//  q2.swift
+//  test_git
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct q2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    q2()
+}
---
+++ test_git/q3.swift
@@ -0,0 +1,18 @@
+//
+//  q3.swift
+//  test_git
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct q3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    q3()
+}
