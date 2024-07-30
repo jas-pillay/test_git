141daed992fe4d9b486adfea02f5bd4bbcf9ab49
jasmyn <jasmynpillay22@gmail.com>
2024-07-30 09:57:44 -0400

    Initial Commit

---
+++ test_git/test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_gitApp.swift
+//  test_git
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
